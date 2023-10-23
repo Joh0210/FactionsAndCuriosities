0f64844d054e7e3e59dd59c3c3b9c566ae851a63
Joh0210 <joh.freund02@gmail.com>
2023-10-23 14:35:03 +0200

    0.0.10 added CreativeModeTab

--- src/main/java/de/joh/fnc/utils/CreativeModeTabInit.java
+++ src/main/java/de/joh/fnc/utils/CreativeModeTabInit.java
@@ -5,6 +5,10 @@ import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * Tabs for the Items of this Mod
+ * @author Joh0210
+ */
 public class CreativeModeTabInit {
     public static final CreativeModeTab FACTIONS_AND_CURIOSITIES = new net.minecraft.world.item.CreativeModeTab("factions_and_curiosities"){
         @Override
