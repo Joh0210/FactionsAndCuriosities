0b4e032c02c3af142f1873756d641051474dfe4c
Joh0210 <joh.freund02@gmail.com>
2023-10-24 10:38:41 +0200

    0.0.11 added MischiefArmor Class

--- src/main/java/de/joh/fnc/item/ItemInit.java
+++ src/main/java/de/joh/fnc/item/ItemInit.java
@@ -1,14 +1,11 @@
 package de.joh.fnc.item;
 
 import de.joh.fnc.FactionsAndCuriosities;
-import de.joh.fnc.item.init.ArmorMaterials;
 import de.joh.fnc.item.init.DebugRod;
 import de.joh.fnc.item.init.Dice;
-import de.joh.fnc.utils.CreativeModeTabInit;
+import de.joh.fnc.item.init.MischiefArmor;
 import net.minecraft.world.entity.EquipmentSlot;
-import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.Rarity;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -24,10 +21,10 @@ public class ItemInit {
     public static final RegistryObject<Item> DEBUG_ROD = ITEMS.register("debug_rod", DebugRod::new);
     public static final RegistryObject<Item> DICE = ITEMS.register("dice_six", () -> new Dice(6));
 
-    public static final RegistryObject<Item> MISCHIEF_HELMET = ITEMS.register("mischief_helmet", () -> new ArmorItem(ArmorMaterials.MISCHIEF, EquipmentSlot.HEAD, new Item.Properties().tab(CreativeModeTabInit.FACTIONS_AND_CURIOSITIES).rarity(Rarity.EPIC)));
-    public static final RegistryObject<Item> MISCHIEF_CHESTPLATE = ITEMS.register("mischief_chestplate", () -> new ArmorItem(ArmorMaterials.MISCHIEF, EquipmentSlot.CHEST, new Item.Properties().tab(CreativeModeTabInit.FACTIONS_AND_CURIOSITIES).rarity(Rarity.EPIC)));
-    public static final RegistryObject<Item> MISCHIEF_LEGGING = ITEMS.register("mischief_legging", () -> new ArmorItem(ArmorMaterials.MISCHIEF, EquipmentSlot.LEGS, new Item.Properties().tab(CreativeModeTabInit.FACTIONS_AND_CURIOSITIES).rarity(Rarity.EPIC)));
-    public static final RegistryObject<Item> MISCHIEF_BOOTS = ITEMS.register("mischief_boots", () -> new ArmorItem(ArmorMaterials.MISCHIEF, EquipmentSlot.FEET, new Item.Properties().tab(CreativeModeTabInit.FACTIONS_AND_CURIOSITIES).rarity(Rarity.EPIC)));
+    public static final RegistryObject<Item> MISCHIEF_HELMET = ITEMS.register("mischief_helmet", () -> new MischiefArmor(EquipmentSlot.HEAD));
+    public static final RegistryObject<Item> MISCHIEF_CHESTPLATE = ITEMS.register("mischief_chestplate", () -> new MischiefArmor(EquipmentSlot.CHEST));
+    public static final RegistryObject<Item> MISCHIEF_LEGGING = ITEMS.register("mischief_legging", () -> new MischiefArmor(EquipmentSlot.LEGS));
+    public static final RegistryObject<Item> MISCHIEF_BOOTS = ITEMS.register("mischief_boots", () -> new MischiefArmor(EquipmentSlot.FEET));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
---
+++ src/main/java/de/joh/fnc/item/init/MischiefArmor.java
@@ -0,0 +1,86 @@
+package de.joh.fnc.item.init;
+
+import com.mna.api.items.IFactionSpecific;
+import com.mna.api.items.ITieredItem;
+import com.mna.items.armor.IBrokenArmorReplaceable;
+import com.mna.items.armor.ISetItem;
+import de.joh.fnc.factions.FactionInit;
+import de.joh.fnc.utils.AttributeInit;
+import de.joh.fnc.utils.CreativeModeTabInit;
+import de.joh.fnc.utils.RLoc;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.AttributeInstance;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.item.*;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+/**
+ * An Armor for the {@link FactionInit#WILD Wilde Courts Faction}
+ * <br>Set Effekts (full armor must be worn):
+ * <br> - +1 Wild Magic Luck
+ * <br> - todo: other stuff
+ * @author Joh0210
+ */
+public class MischiefArmor extends ArmorItem implements ISetItem, ITieredItem<MischiefArmor>, IFactionSpecific, IBrokenArmorReplaceable<MischiefArmor> {
+    private static final AttributeModifier LUCK_BOOST = new AttributeModifier("mischief_armor_set_bonus", 1, AttributeModifier.Operation.ADDITION);
+    private int tier = -1;
+    private static final ResourceLocation mischief_armor_set_bonus = RLoc.create("mischief_armor_set_bonus");
+
+    public MischiefArmor(EquipmentSlot pSlot) {
+        super(ArmorMaterials.MISCHIEF, pSlot, new Item.Properties().tab(CreativeModeTabInit.FACTIONS_AND_CURIOSITIES).rarity(Rarity.EPIC));
+    }
+
+    @Override
+    public void applySetBonus(LivingEntity living, EquipmentSlot... setSlots) {
+        AttributeInstance wild_magic_luck = living.getAttribute(AttributeInit.WILD_MAGIC_LUCK.get());
+        if(wild_magic_luck != null && !wild_magic_luck.hasModifier(LUCK_BOOST)){
+            wild_magic_luck.addTransientModifier(LUCK_BOOST);
+        }
+    }
+
+    @Override
+    public void removeSetBonus(LivingEntity living, EquipmentSlot... setSlots) {
+        AttributeInstance wild_magic_luck = living.getAttribute(AttributeInit.WILD_MAGIC_LUCK.get());
+        if(wild_magic_luck != null){
+            wild_magic_luck.removeModifier(LUCK_BOOST);
+        }
+    }
+
+    @Override
+    public void appendHoverText(@NotNull ItemStack stack, Level worldIn, @NotNull List<Component> tooltip, @NotNull TooltipFlag flagIn) {
+        ISetItem.super.addSetTooltip(tooltip);
+        super.appendHoverText(stack, worldIn, tooltip, flagIn);
+    }
+
+    @Override
+    public void setCachedTier(int tier) {
+        this.tier = tier;
+    }
+
+    @Override
+    public int getCachedtier() {
+        return this.tier;
+    }
+
+    @Override
+    public ResourceLocation getSetIdentifier() {
+        return mischief_armor_set_bonus;
+    }
+
+    @Override
+    public ResourceLocation getFaction() {
+        return FactionInit.WILD.getRegistryName();
+    }
+
+    @Override
+    public <T extends LivingEntity> int damageItem(ItemStack stack, int amount, T entity, Consumer<T> onBroken) {
+        return super.damageItem(stack, amount * 3, entity, onBroken);
+    }
+}
